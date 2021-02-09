d015edb7c949d37e5d3867f58f8d323086c74e66
Ove3r <74508068+Ove3r@users.noreply.github.com>
2021-02-09 14:47:31 -0500

    Added UUID Command
    Includes custom errors.

--- cogs/player.py
+++ cogs/player.py
@@ -1,14 +1,27 @@
 import discord
-from discord.ext import tasks, commands
+from discord.ext import commands
 import requests, json
 from helpers import key
+from helpers.errors import *
+from helpers.utils import *
 
 class Player(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(name="uuid")
+    async def fetch_uuid(self, ctx, name):
+        try:
+            uuid = getUUID(name)
+            embed=discord.Embed(title=f"{name}", description=uuid, color=0xdc6565)
+            embed.set_footer(text="Stella Bot by Over#6203")
+            embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
 
-
+        except PlayerNotFound:
+            embed=discord.Embed(title="Error ◆ PlayerNotFound", description=f"User `{name}` not found.", color=0xdc6565)
+            embed.set_footer(text=f"Stella Bot by Over#6203 ")
+            embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Player(bot))
--- helpers/utils.py
+++ helpers/utils.py
@@ -1,6 +1,15 @@
-import helpers.key
 import requests
+import helpers.key
+from helpers.errors import *
 
 def getName(uuid):
     data = requests.get(f"https://api.mojang.com/user/profiles/{uuid}/names").json()
     return data[-1]["name"]
+
+def getUUID(name):
+    try:
+        data = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{name}").json()
+        uuid = data["id"]
+        return uuid
+    except:
+        raise PlayerNotFound
