3619aac137de9a378ceefa04e78ba2e83f868bfb
Ove3r <74508068+Ove3r@users.noreply.github.com>
2021-02-24 16:54:35 -0500

    Improved Readability of Fishing Tab

--- constants/constants_fishing.py
+++ constants/constants_fishing.py
@@ -4,7 +4,7 @@ def FISHING_STATS():
 			"api_name":[
 				"kills_pond_squid"
 			],
-			"common_name":"<:squid:814192373771927642> Squid",
+			"common_name":"<:squid:814192373771927642> **Squid**",
 			"count":0,
 			"category":"Standard"
 		},
@@ -12,7 +12,7 @@ def FISHING_STATS():
 			"api_name":[
 				"kills_sea_walker"
 			],
-			"common_name":"<:sea_walker:814192444555788309> Sea Walker",
+			"common_name":"<:sea_walker:814192444555788309> **Sea Walker**",
 			"count":0,
 			"category":"Standard"
 		},
@@ -20,7 +20,7 @@ def FISHING_STATS():
 			"api_name":[
 				"kills_night_squid"
 			],
-			"common_name":"<:squid:814192373771927642> Night Squid",
+			"common_name":"<:squid:814192373771927642> **Night Squid**",
 			"count":0,
 			"category":"Standard"
 		},
@@ -28,7 +28,7 @@ def FISHING_STATS():
 			"api_name":[
 				"kills_sea_guardian"
 			],
-			"common_name":"<:guardian:814191282582323201> Sea Guardian",
+			"common_name":"<:guardian:814191282582323201> **Sea Guardian**",
 			"count":0,
 			"category":"Standard"
 		},
@@ -36,7 +36,7 @@ def FISHING_STATS():
 			"api_name":[
 				"kills_sea_witch"
 			],
-			"common_name":"<:sea_witch:814192263068123136> Sea Witch",
+			"common_name":"<:sea_witch:814192263068123136> **Sea Witch**",
 			"count":0,
 			"category":"Standard"
 		},
@@ -44,7 +44,7 @@ def FISHING_STATS():
 			"api_name":[
 				"kills_sea_archer"
 			],
-			"common_name":"<:sea_archer:814192174060404787> Sea Archer",
+			"common_name":"<:sea_archer:814192174060404787> **Sea Archer**",
 			"count":0,
 			"category":"Standard"
 		},
@@ -53,7 +53,7 @@ def FISHING_STATS():
 				"kills_zombie_deep",
 				"kills_chicken_deep"
 			],
-			"common_name":"<:monster_of_the_deep:814191924604436520> Monster of the Deep",
+			"common_name":"<:monster_of_the_deep:814191924604436520> **Monster of the Deep**",
 			"count":0,
 			"category":"Standard"
 		},
@@ -61,7 +61,7 @@ def FISHING_STATS():
 			"api_name":[
 				"kills_catfish"
 			],
-			"common_name":"<:cat_fish:814191813149458523> Catfish",
+			"common_name":"<:cat_fish:814191813149458523> **Catfish**",
 			"count":0,
 			"category":"Standard"
 		},
@@ -69,7 +69,7 @@ def FISHING_STATS():
 			"api_name":[
 				"kills_carrot_king"
 			],
-			"common_name":"<:carrot_king:814191700854439996> Carrot King",
+			"common_name":"<:carrot_king:814191700854439996> **Carrot King**",
 			"count":0,
 			"category":"Standard"
 		},
@@ -77,7 +77,7 @@ def FISHING_STATS():
 			"api_name":[
 				"kills_sea_leech"
 			],
-			"common_name":"<:sea_leech:814191387490123856> Sea Leech",
+			"common_name":"<:sea_leech:814191387490123856> **Sea Leech**",
 			"count":0,
 			"category":"Standard"
 		},
@@ -85,7 +85,7 @@ def FISHING_STATS():
 			"api_name":[
 				"kills_guardian_defender"
 			],
-			"common_name":"<:guardian:814191282582323201> Guardian Defender",
+			"common_name":"<:guardian:814191282582323201> **Guardian Defender**",
 			"count":0,
 			"category":"Standard"
 		},
@@ -93,7 +93,7 @@ def FISHING_STATS():
 			"api_name":[
 				"kills_deep_sea_protector"
 			],
-			"common_name":"<:deep_sea_protector:814191157155725322> Deep Sea Protector",
+			"common_name":"<:deep_sea_protector:814191157155725322> **Deep Sea Protector**",
 			"count":0,
 			"category":"Standard"
 		},
@@ -101,7 +101,7 @@ def FISHING_STATS():
 			"api_name":[
 				"kills_water_hydra"
 			],
-			"common_name":"<:water_hydra:802728737761656842> Water Hydra",
+			"common_name":"<:water_hydra:802728737761656842> **Water Hydra**",
 			"count":0,
 			"category":"Standard"
 		},
@@ -110,7 +110,7 @@ def FISHING_STATS():
 				"kills_skeleton_emperor",
 				"kills_guardian_emperor"
 			],
-			"common_name":"<:sea_emperor:802729492068958259> Sea Emperor",
+			"common_name":"<:sea_emperor:802729492068958259> **Sea Emperor**",
 			"count":0,
 			"category":"Standard"
 		},
@@ -118,7 +118,7 @@ def FISHING_STATS():
 			"api_name":[
 				"kills_scarecrow"
 			],
-			"common_name":"<:scarecrow:802732647310032917> Scarecrow",
+			"common_name":"<:scarecrow:802732647310032917> **Scarecrow**",
 			"count":0,
 			"category":"Spooky"
 		},
@@ -126,7 +126,7 @@ def FISHING_STATS():
 			"api_name":[
 				"kills_nightmare"
 			],
-			"common_name":"<:nightmare:802732791556997120> Nightmare",
+			"common_name":"<:nightmare:802732791556997120> **Nightmare**",
 			"count":0,
 			"category":"Spooky"
 		},
@@ -134,7 +134,7 @@ def FISHING_STATS():
 			"api_name":[
 				"kills_werewolf"
 			],
-			"common_name":"<:werewolf:802732906513956865> Werewolf",
+			"common_name":"<:werewolf:802732906513956865> **Werewolf**",
 			"count":0,
 			"category":"Spooky"
 		},
@@ -142,7 +142,7 @@ def FISHING_STATS():
 			"api_name":[
 				"kills_phantom_fisherman"
 			],
-			"common_name":"<:phantom_fisher:802733007248818206> Phantom Fisher",
+			"common_name":"<:phantom_fisher:802733007248818206> **Phantom Fisher**",
 			"count":0,
 			"category":"Spooky"
 		},
@@ -150,7 +150,7 @@ def FISHING_STATS():
 			"api_name":[
 				"kills_grim_reaper"
 			],
-			"common_name":"<:grim_reaper:802733102166048808> Grim Reaper",
+			"common_name":"<:grim_reaper:802733102166048808> **Grim Reaper**",
 			"count":0,
 			"category":"Spooky"
 		},
@@ -158,7 +158,7 @@ def FISHING_STATS():
 			"api_name":[
 				"kills_frozen_steve"
 			],
-			"common_name":"<:frozen_steve:802729900430589992> Frozen Steve",
+			"common_name":"<:frozen_steve:802729900430589992> **Frozen Steve**",
 			"count":0,
 			"category":"Winter"
 		},
@@ -166,7 +166,7 @@ def FISHING_STATS():
 			"api_name":[
 				"kills_frosty_the_snowman"
 			],
-			"common_name":"<:frosty:802730029283934251> Frosty",
+			"common_name":"<:frosty:802730029283934251> **Frosty**",
 			"count":0,
 			"category":"Winter"
 		},
@@ -174,7 +174,7 @@ def FISHING_STATS():
 			"api_name":[
 				"kills_grinch"
 			],
-			"common_name":"<:grinch:802730176567312465> Grinch",
+			"common_name":"<:grinch:802730176567312465> **Grinch**",
 			"count":0,
 			"category":"Winter"
 		},
@@ -182,7 +182,7 @@ def FISHING_STATS():
 			"api_name":[
 				"kills_yeti"
 			],
-			"common_name":"<:yeti:802730361704284210> Yeti",
+			"common_name":"<:yeti:802730361704284210> **Yeti**",
 			"count":0,
 			"category":"Winter"
 		},
@@ -190,7 +190,7 @@ def FISHING_STATS():
 			"api_name":[
 				"kills_nurse_shark"
 			],
-			"common_name":"<:nurse_shark:802733704740077598> Nurse Shark",
+			"common_name":"<:nurse_shark:802733704740077598> **Nurse Shark**",
 			"count":0,
 			"category":"Marina"
 		},
@@ -198,7 +198,7 @@ def FISHING_STATS():
 			"api_name":[
 				"kills_blue_shark"
 			],
-			"common_name":"<:blue_shark:802733823921225728> Blue Shark",
+			"common_name":"<:blue_shark:802733823921225728> **Blue Shark**",
 			"count":0,
 			"category":"Marina"
 		},
@@ -206,7 +206,7 @@ def FISHING_STATS():
 			"api_name":[
 				"kills_tiger_shark"
 			],
-			"common_name":"<:tiger_shark:802733930285367306> Tiger Shark",
+			"common_name":"<:tiger_shark:802733930285367306> **Tiger Shark**",
 			"count":0,
 			"category":"Marina"
 		},
@@ -214,7 +214,7 @@ def FISHING_STATS():
 			"api_name":[
 				"kills_great_white_shark"
 			],
-			"common_name":"<:great_white_shark:802734017685487616> Great White Shark",
+			"common_name":"<:great_white_shark:802734017685487616> **Great White Shark**",
 			"count":0,
 			"category":"Marina"
 		}
