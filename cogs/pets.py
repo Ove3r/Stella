d80e5f3015cc27977901d3eac3a9c4b82e260e76
Ove3r <74508068+Ove3r@users.noreply.github.com>
2021-03-02 16:48:25 -0500

    Added Terminal Tracking For Pets Command

--- cogs/pets.py
+++ cogs/pets.py
@@ -8,9 +8,9 @@ from constants.constants_pets import GET_PETS_CONSTANTS
 class Pets(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
+
     def pets_flip(self, PETS_CONSTANTS):
-        #Db load
+        print(f"{ctx.author.name} sent pets command in {ctx.guild}")
         with open("data/ah.json") as database:
             database = json.load(database)
 
@@ -23,7 +23,7 @@ class Pets(commands.Cog):
                         if check["maxed"] in entry["extra"]:
                             check["maxed_list"].append(entry["starting_bid"])
                         break
-                
+
         messages = {
             "farming" : "",
             "mining" : "",
@@ -52,7 +52,7 @@ class Pets(commands.Cog):
     async def pets_calculator(self,ctx):
         def check(reaction, member): #For Tab Menu
             return member == ctx.author
-        
+
         def tabs_embed(message, category, thumbnail): #For creating new embeds for each category
             embed=discord.Embed(title=f"Pets Leveling", description=f"Profit from leveling **{rarity}** pets from cheapest BIN to lvl 100.", color=0xdc6565)
             embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
@@ -74,21 +74,21 @@ class Pets(commands.Cog):
         await output.add_reaction("<:jungle_sapling:810605504934051892>")
         await output.add_reaction("<:fishing:801090235542929448>")
         await output.add_reaction("<:spawn_egg:810606172997812258>")
-        
+
         #Tab Menu
         reaction = None
         while True:
             if str(reaction) == "<:golden_hoe:801205315806167050>":
                 await output.edit(embed=tabs_embed(messages["farming"],"Farming","https://static.wikia.nocookie.net/hypixel-skyblock/images/a/af/Elephant_Pet.png/revision/latest?cb=20200506101231"))
-            elif str(reaction) == "<:stone_pickaxe:810604855726571545>": 
+            elif str(reaction) == "<:stone_pickaxe:810604855726571545>":
                 await output.edit(embed=tabs_embed(messages["mining"],"Mining","https://static.wikia.nocookie.net/hypixel-skyblock/images/0/0c/Silverfish_Pet.png/revision/latest?cb=20200222204846"))
-            elif str(reaction) == "<:stone_sword:810605120752058408>": 
+            elif str(reaction) == "<:stone_sword:810605120752058408>":
                 await output.edit(embed=tabs_embed(messages["combat"],"Combat","https://static.wikia.nocookie.net/hypixel-skyblock/images/f/f2/Wolf_Pet.png/revision/latest?cb=20200222204848"))
-            elif str(reaction) == "<:jungle_sapling:810605504934051892>": 
+            elif str(reaction) == "<:jungle_sapling:810605504934051892>":
                 await output.edit(embed=tabs_embed(messages["foraging"],"Foraging","https://static.wikia.nocookie.net/hypixel-skyblock/images/e/e9/Ocelot_Pet.png/revision/latest?cb=20200222204840"))
-            elif str(reaction) == "<:fishing:801090235542929448>": 
+            elif str(reaction) == "<:fishing:801090235542929448>":
                 await output.edit(embed=tabs_embed(messages["fishing"],"Fishing","https://static.wikia.nocookie.net/hypixel-skyblock/images/e/ec/Flying_Fish_Pet.png/revision/latest?cb=20200222204853"))
-            elif str(reaction) == "<:spawn_egg:810606172997812258>": 
+            elif str(reaction) == "<:spawn_egg:810606172997812258>":
                 await output.edit(embed=tabs_embed(messages["other"],"Enchanting/Alchemy","https://static.wikia.nocookie.net/hypixel-skyblock/images/4/47/Parrot_Pet.png/revision/latest?cb=20200222204854"))
             try: #Timeout
                 reaction, member = await self.bot.wait_for("reaction_add", timeout=45.0, check=check)
@@ -97,7 +97,7 @@ class Pets(commands.Cog):
                 break
         await output.clear_reactions()
         await output.add_reaction("🛑")
-        
+
 
 def setup(bot):
     bot.add_cog(Pets(bot))
