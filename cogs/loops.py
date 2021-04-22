8e68bef187d688acf8876af3a0f8b7f0e721e3df
Ove3r <74508068+Ove3r@users.noreply.github.com>
2021-04-21 21:55:31 -0400

    Fixed Typo

--- cogs/loops.py
+++ cogs/loops.py
@@ -48,7 +48,7 @@ class Loops(commands.Cog):
             if get_player_status(entry["uuid"]) != entry["location"]:
                 user = self.bot.get_user(int(entry["discord_id"]))
                 embed=discord.Embed(title="AFK Tracker", description=f"`{entry['player']}` is no longer on `{entry['location']}`. \n\nTracking for `{entry['player']}` has been removed. \nUse **stella afk `{entry['player']}`** to track again.", color=0xdc6565)
-                embed.add_field(name="Tracking Time", value=f"**{ms_to_standard(time.time() - entry['time'])}**")
+                embed.add_field(name="Tracking Time", value=f"**{ms_to_standard(time.time() - entry['starting_time'])}**")
                 embed.set_thumbnail(url=f"https://visage.surgeplay.com/bust/{entry['uuid']}")
                 embed.set_footer(text="Stella Bot by Over#6203")
 
